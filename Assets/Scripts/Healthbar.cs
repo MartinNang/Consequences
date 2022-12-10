3c83817feb66e457aaa810e36259266880470b6e
MartinNang <m.nangole@gmx.at>
2022-12-10 14:03:07 +0100

    Create Healthbar.cs

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Healthbar : MonoBehaviour
+{
+    public GameObject player;
+    private float playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //render health bar
+
+    }
+
+    void setHealth(float health)
+    {
+        playerHealth = health;
+    }
+}
