6c284ebc9ccef37717e9dad53f4dfd9374195c28
Flammentrieb <flammentrieb@gmail.com>
2022-12-10 21:42:23 +0100

    PlayerStatus

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    
+    public bool hasMelee = false;
+    public bool hasLongRange = false;
+    public bool hasXAxis = true;
+    public bool hasYAxis = true;
+    public bool hasHPWeapon = false;
+    public bool hasTimerShield = false;
+    public bool hasDash = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
